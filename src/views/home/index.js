6739cb516daf432fb978313c04692751e3312f6c
Qiulonghui <569374939@qq.com>
2020-04-13 17:27:22 +0800

    update: 开发基础配置完成

---
+++ src/views/home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { Button } from 'antd-mobile';
+import {HomeWrapper} from './style'
+
+function Home() {
+  return (
+    <HomeWrapper>
+     Home
+    </HomeWrapper>
+  );
+}
+
+export default Home;
---
+++ src/views/home/style.js
@@ -0,0 +1,5 @@
+import styled from 'styled-px2vw'
+
+export const HomeWrapper = styled.div`
+	font-size: 24px;
+`
